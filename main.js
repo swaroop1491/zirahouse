7bd40178d0d32f8232fb600c58124ec283e6a7b9
swaroop1491 <snaik303@gmail.com>
2025-09-20 11:27:14 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+fetch('data.json')
+    .then(response => response.json())
+    .then(properties => {
+        const list = document.getElementById('propertyList');
+        properties.forEach(property => {
+            const li = document.createElement('li');
+            li.className = 'property-item';
+            li.innerHTML = `
+                <span class="property-title">${property.title}</span>
+                <div class="property-details">
+                    Location: ${property.location}<br>
+                    Price: ${property.price}<br>
+                    Bedrooms: ${property.bedrooms}
+                </div>
+            `;
+            list.appendChild(li);
+        });
+    })
+    .catch(error => {
+        document.getElementById('propertyList').innerHTML = '<li>Error
